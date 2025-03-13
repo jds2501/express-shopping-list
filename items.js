aa10ad98089abb9e8ee1110370ee4ce50a661f6e
Jason Smith <jasondsmith2501@gmail.com>
2025-03-12 17:42:28 -0700

    Added basic get request

--- items.js
+++ items.js
@@ -1,8 +1,9 @@
 const express = require('express');
+const db = require('./fakeDB.js');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send("test");
+    res.json(db);
 })
 
 module.exports = router;
