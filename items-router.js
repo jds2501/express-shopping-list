84842205db536f553a27c3196fd27866146f527d
Jason Smith <jasondsmith2501@gmail.com>
2025-03-13 10:25:01 -0700

    Added post request

--- items-router.js
+++ items-router.js
@@ -6,4 +6,14 @@ router.get('/', (req, res) => {
     return res.json(itemsModel.getItems());
 });
 
+router.post('/', (req, res) => {
+    try {
+        itemsModel.addItem(req.body.name, req.body.price);
+        return res.json({ "added": { name: req.body.name, price: req.body.price } });
+    } catch (err) {
+        res.status = 400;
+        return res.json({ "error": err.toString() });
+    }
+})
+
 module.exports = router;
