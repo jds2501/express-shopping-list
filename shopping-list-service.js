cc8e51cdc9121fb3b60d853ed524b6c9bce1f68e
Jason Smith <jasondsmith2501@gmail.com>
2025-03-13 13:39:28 -0700

    Added 404 handling

--- shopping-list-service.js
+++ shopping-list-service.js
@@ -5,11 +5,19 @@ const app = express();
 
 app.use(express.json());
 app.use('/items', items);
+
+// Handles accessing a non-matching path
+app.use((req, res) => {
+    return res.status(400).send({
+        error: "URL not found"
+    });
+});
+
+// Handles exceptions
 app.use((err, req, res, next) => {
-    res.status(err.statusCode || 500).send({
+    return res.status(err.statusCode || 500).send({
         error: err.message || "Internal server error"
     });
-    next();
 });
 
 app.listen(3000, () => {
