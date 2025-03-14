e6a7c7cc32591d20772b7731a6c794df5825d10b
Jason Smith <jasondsmith2501@gmail.com>
2025-03-14 09:31:11 -0700

    Setup basic API test

--- __test__/shopping-list-service.test.js
+++ __test__/shopping-list-service.test.js
@@ -1,5 +1,21 @@
 const request = require('supertest');
 
 describe('Shopping List Service API Tests', () => {
+    let server;
 
+    beforeEach(() => {
+        const createServer = require('../shopping-list-service.js');
+        const app = createServer();
+        server = app.listen();
+    });
+
+    afterEach((done) => {
+        server.close(done);
+    });
+
+    test('GET request on /items with no items', (done) => {
+        request(server)
+            .get("/items")
+            .expect(200, done);
+    })
 });
