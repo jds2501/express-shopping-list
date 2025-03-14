147f1cb9f287975107a75b10f46c03486f1af4a9
Jason Smith <jasondsmith2501@gmail.com>
2025-03-13 22:17:52 -0700

    Added more unit tests

--- __test__/items-model.test.js
+++ __test__/items-model.test.js
@@ -12,4 +12,15 @@ describe('Items Model', () => {
     it('get item throws error on item that does not exist', () => {
         expect(() => itemsModel.getItem("dne")).toThrow("dne not found in DB");
     });
+
+    it('adding an item should be possible to get from get items and get item', () => {
+        itemsModel.addItem("test", 2.3);
+        expect(itemsModel.getItems()[0]).toStrictEqual({ name: "test", price: 2.3 });
+        expect(itemsModel.getItem("test")).toStrictEqual({ name: "test", price: 2.3 });
+    });
+
+    it('missing name and/or price should throw an error', () => {
+        expect(() => itemsModel.addItem()).toThrow("A string name needs to be included");
+        expect(() => itemsModel.addItem("test")).toThrow("A numerical price needs to be included");
+    })
 });
