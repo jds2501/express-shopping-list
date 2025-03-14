5b9ae47a15d048c00e678e531ac3006051a299e9
Jason Smith <jasondsmith2501@gmail.com>
2025-03-14 13:44:00 -0700

    Added a post & get request test

--- __test__/shopping-list-service.test.js
+++ __test__/shopping-list-service.test.js
@@ -24,4 +24,23 @@ describe('Shopping List Service API Tests', () => {
             .get("/dne")
             .expect(404, { error: "URL not found" }, done);
     });
+
+    test('POST and GET request on items', (done) => {
+        request(server)
+            .post('/items')
+            .send({ name: "test", price: 2.3 })
+            .expect(200)
+            .then(response => {
+                expect(response.body.added).toStrictEqual({ name: "test", price: 2.3 });
+                done();
+            });
+
+        request(server)
+            .get("/items")
+            .expect(200, [{ name: "test", price: 2.3 }], done);
+
+        request(server)
+            .get("/items/test")
+            .expect(200, { name: "test", price: 2.3 }, done);
+    });
 });
