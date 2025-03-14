a0e1e03809ce45a278f9c6ef1356cd0ccd55b24e
Jason Smith <jasondsmith2501@gmail.com>
2025-03-14 13:26:51 -0700

    Added some basic API tests

--- __test__/shopping-list-service.test.js
+++ __test__/shopping-list-service.test.js
@@ -17,5 +17,11 @@ describe('Shopping List Service API Tests', () => {
         request(server)
             .get("/items")
             .expect(200, [], done);
-    })
+    });
+
+    test('GET request to non-supported URL throws 404', (done) => {
+        request(server)
+            .get("/dne")
+            .expect(404, { error: "URL not found" }, done);
+    });
 });
